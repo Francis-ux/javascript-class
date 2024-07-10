f614b9bfce3021ad0e40a5e1f0a6ddc9d3bf8189
francis-ux <kingmicheal744@gmail.com>
2024-07-10 23:35:24 +0100

    commit

---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,43 @@
+// Data Types
+/*
+A value in JavaScript has a data type and it always of a certain type.For example,a string or a number.
+*/
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+// Number
+/*
+The number type represent both integer and floating point numbers
+*/
+
+let n = 123;
+n = 12.345;
+
+console.log(n);
+
+// BigInt
+/*
+The BigInt type can be used to represent arbitrarily large integers
+*/
+
+let big = 1234567890123456789012345678901234567890n;
+console.log(big);
+
+// The "n" at the end means it's a BigInt
+
+// String
+/*
+The string type represents a sequence of characters
+A string in Javascript must be enclosed in single or double quotes
+*/
+
+let name = 'John';
+name = 'Jane';
+
+let calculation = 1 + 2;
+console.log(calculation);
