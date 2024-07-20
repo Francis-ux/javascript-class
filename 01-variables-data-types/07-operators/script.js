f54906ad21996b0584302edbd95071b55aefc4bc
francis-ux <kingmicheal744@gmail.com>
2024-07-20 17:54:48 +0100

    commit

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,58 @@
+// Operators
+/*
+1. Arithmetic
+2. Assignment
+3. Comparison
+4. Logical
+5. Bitwise
+
+1. Arithmetic
+
+Arithmetic operators are used to perform mathematical operations on numbers.
+Examples of arithmetic operators are:
+
+Addition: +
+Subtraction: -
+Multiplication: *
+Division: /
+Modulus: %
+Increment: ++
+Decrement: --
+
+2. Assignment
+
+Assignment operators are used to assign values to variables.
+Examples of assignment operators are:
+
+Equality: =
+Addition assignment: +=
+Subtraction assignment: -=
+Multiplication assignment: *=
+Division assignment: /=
+Modulus assignment: %=
+Increment assignment: +=
+Decrement assignment: -=
+
+3. Comparison
+
+Comparison operators are used to compare two values.
+Examples of comparison operators are:
+
+Equal to: ==
+Not equal to: !=
+Greater than: >
+Less than: <
+Greater than or equal to: >=
+Less than or equal to: <=
+Logical AND: &&
+Logical OR: ||
+*/
+
+let a = 10;
+let b = 5;
+
+if (a == b || a > b) {
+  console.log('Yes');
+}else{
+  console.log('No');
+}
