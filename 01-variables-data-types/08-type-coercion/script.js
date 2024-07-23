488106fc2ed738633d157a7a86354327bd92034b
francis-ux <kingmicheal744@gmail.com>
2024-07-23 21:44:30 +0100

    commit

---
+++ 01-variables-data-types/08-type-coercion/script.js
@@ -0,0 +1,12 @@
+// Type Coercion
+
+// Type coercion is the automatic conversion of one data type to another.
+
+// Coerced to a string
+x = 5 + '5';
+console.log(typeof x, x);
+
+// Coerced to a number
+x = 5 * '5';
+x = 7 + '5';
+console.log(typeof x, x)
