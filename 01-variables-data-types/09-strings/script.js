2f432e7b623352e92690c6581772d73c6aaf7e46
francis-ux <kingmicheal744@gmail.com>
2024-07-28 18:37:47 +0100

    commit

--- 01-variables-data-types/09-strings/script.js
+++ 01-variables-data-types/09-strings/script.js
@@ -24,4 +24,40 @@ x = typeof s;
 
 x = s.length;
 
+// Access value by key
+
+x = s[6];
+
+// Show the prototype of the String object
+x = s.__proto__;
+
+// Change case
+x = s.toUpperCase();
+x = s.toLowerCase();
+
+// indexOf -  returns the indexof the first occurrence of a specific value in a string
+x = s.indexOf('o');
+
+// subString() - search a string for a specified value and returns the part of the string that matches the value.
+x = s.substring(6, 11);
+
+// Slice() - extracts a part of a string and returns the extracted part in a new string.
+x = s.slice(0, 6); // To continue
+
+// trim() - remove whitespace from both sides of a string
+x = '          Micheal        ';
+x = x.trim();
+
+// replace() - replace all instances of a string
+x = s.replace('World', 'Universe');
+
+// includes() - return true if the string is found
+x = s.includes('World');
+
+// valueOf() - returns the primitive value of a string
+x = s.valueOf();
+
+// split() - returns and array of strings
+x = s.split('');
+
 console.log(x);
