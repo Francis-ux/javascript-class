fe761432ed8c6c17252f434c3cb12f5c462445a1
francis-ux <kingmicheal744@gmail.com>
2024-08-07 21:44:54 +0100

    commit

---
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,15 @@
+let x;
+
+// Array Literal
+const numbers = [12, 45, 29, 39, 102];
+const mixed = [12, 'Hello', true, null, { a: 1, b: 2 }, [1, 2, 3]];
+
+// Array Constructor
+const fruits = new Array('Apple', 'Orange', 'Mango');
+
+// Get value by index
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+console.log(x);
