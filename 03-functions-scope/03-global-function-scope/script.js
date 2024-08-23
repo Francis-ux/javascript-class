a9fd87620bf78afd3bb8b909f9e5cdd85bd06671
francis-ux <kingmicheal744@gmail.com>
2024-08-23 21:21:11 +0100

    commit

---
+++ 03-functions-scope/03-global-function-scope/script.js
@@ -0,0 +1,31 @@
+// 'window' is globally scoped
+// console.log(window);
+// alert('Hello World');
+// console.log(innerWidth);
+
+// Global Scope Variable
+const x = 100;
+
+console.log(x, 'in global');
+
+function run() {
+  // Access global vars in functions
+  console.log(window.innerHeight);
+  console.log(x, 'in function');
+}
+
+run();
+
+// Access global vars in blocks
+if (true) {
+  console.log(x, 'in block');
+}
+
+function add() {
+  // Overwriting global x (variable shadowing)
+  const x = 1;
+  const y = 50;
+  console.log(x + y);
+}
+
+add();
