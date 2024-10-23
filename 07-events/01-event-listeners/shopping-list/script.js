a32486f57b9aecd7d4a3fd49df710e8176f9ec41
francis-ux <kingmicheal744@gmail.com>
2024-10-23 22:04:13 +0100

    commit

---
+++ 07-events/01-event-listeners/shopping-list/script.js
@@ -0,0 +1,30 @@
+const clearBtn = document.querySelector('#clear');
+
+function onClear() {
+  const itemList = document.querySelector('ul');
+  const items = itemList.querySelectorAll('li');
+
+  itemList.innerHTML = '';
+
+  items.forEach((item) => {
+    item.remove();
+  });
+}
+
+// Javascript Event Listener
+// clearBtn.onclick = function () {
+//   alert('Clear Items');
+// };
+// clearBtn.onclick = function () {
+//   console.log('Clear Items');
+// };
+
+// addEventListener()
+// clearBtn.addEventListener('click', () => alert('Clear Items'));
+
+// Use named function
+clearBtn.addEventListener('click', onClear);
+
+// removeEventListener()
+setTimeout(() => clearBtn.removeEventListener('click', onClear), 3000);
+
---
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -0,0 +1,22 @@
+const logo = document.querySelector('img');
+
+const onClick = () => console.log('click event');
+
+const onDoubleClick = () => {
+  if (document.body.style.backgroundColor !== 'purple') {
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+};
+const onRightClick = () => console.log('right click event');
+
+const onMouseDown = () => console.log('mouse down event');
+
+// Event Listeners
+logo.addEventListener('click', onClick);
+logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDown);
