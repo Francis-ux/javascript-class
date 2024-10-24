06d5a9d4bec167dc2f38d8cdcf380415f9566ae8
francis-ux <kingmicheal744@gmail.com>
2024-10-24 21:29:26 +0100

    commit

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -12,11 +12,17 @@ const onDoubleClick = () => {
   }
 };
 const onRightClick = () => console.log('right click event');
-
 const onMouseDown = () => console.log('mouse down event');
-
+const onMouseUp = () => console.log('mouse up event');
+const onMouseWheel = () => console.log('mouse wheel event');
+const onMouseOver = () => console.log('mouse over event');
+const onMouseOut = () => console.log('mouse out event');
 // Event Listeners
 logo.addEventListener('click', onClick);
 logo.addEventListener('dblclick', onDoubleClick);
 logo.addEventListener('contextmenu', onRightClick);
 logo.addEventListener('mousedown', onMouseDown);
+logo.addEventListener('mouseup', onMouseUp);
+logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
---
+++ 07-events/04-keyboard-events/shopping-list/script.js
@@ -0,0 +1,15 @@
+const itemInput = document.getElementById('item-input');
+
+const onKeyPress = (e) => {
+  console.log('keypress');
+};
+const onKeyUp = (e) => {
+  console.log('keyup');
+};
+const onKeyDown = (e) => {
+  console.log('keydown');
+};
+
+itemInput.addEventListener('keypress', onKeyPress);
+itemInput.addEventListener('keyup', onKeyUp);
+itemInput.addEventListener('keydown', onKeyDown);
